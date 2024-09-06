1751a7fc9990802c9836f0614d8fb002f0b0f140
Nitesh Kumar <niteshkumar@Niteshs-MacBook-Air.local>
2024-09-06 22:38:59 +0530

    update2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import AddTodo from "@/components/todos/AddTodo";
 import Todo from "@/components/todos/Todo";
 import { prisma } from "@/utils/prisma";
+import { todoProps } from "@/types";
 
-async function getData() {
-
-  //prisma call
+// Fetch data from the database
+async function getData(): Promise<todoProps[]> {
+  // Prisma call to fetch todos
   const data = await prisma.todo.findMany({
     select: {
       title: true,
       id: true,
       isCompleted: true,
+      createdAt: true, // Include createdAt if needed for ordering
     },
     orderBy: {
       createdAt: "desc",
@@ -20,21 +22,20 @@ async function getData() {
 }
 
 export default async function Home() {
-
-  //get data
+  // Fetch data
   const data = await getData();
 
   return (
-    <div className="todo w-screen py-20 flex justify-center flex-col items-center sm: h-full">
+    <div className="todo w-screen py-20 flex justify-center flex-col items-center sm:h-full">
       <span className="text-4xl font-extrabold uppercase mb-10">Todo App</span>
-  
+
       <div className="flex justify-center flex-col items-center">
         <AddTodo />
 
-        {/* show list of input */}
+        {/* Show list of todos */}
         <div className="flex flex-col gap-5 items-center justify-center mt-10 w-screen">
-          {data.map((todo, id) => (
-            <div className="w-full" key={id}>
+          {data.map((todo) => (
+            <div className="w-full" key={todo.id}>
               <Todo todo={todo} />
             </div>
           ))}
